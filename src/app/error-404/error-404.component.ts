06df2feed0ee9100fac1a68147b99c577147e546
philippe.machado <philippe.machado@dmcard.com.br>
2022-03-24 21:30:16 -0300

    Trabalhando com rotas

---
+++ src/app/error-404/error-404.component.ts
@@ -0,0 +1,11 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './error-404.component.html',
+    styleUrls: ['./error-404.component.css']
+    
+})
+export class Error404Component{
+
+}
