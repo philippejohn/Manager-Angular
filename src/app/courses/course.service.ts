4041ef0a0797e23c8e519f47c148e48eda7661b3
philippe.machado <philippe.machado@dmcard.com.br>
2022-03-24 22:49:27 -0300

    Formulario

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -10,6 +10,18 @@ export class CourseService {
    retrieveAll(): Course[]{
        return COURSES;
    }
+
+
+   retriveById(id: number): Course {
+       return COURSES.find((courseInterator: Course) => courseInterator.id === id);
+   }
+
+   save(course: Course): void {
+      if(course.id){
+          const index = COURSES.findIndex((courseInterator: Course) => courseInterator.id === course.id);
+          COURSES[index] = course; 
+      }
+   }
 }
 
 var COURSES: Course[] = [
